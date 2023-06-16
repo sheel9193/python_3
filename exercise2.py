7422e50af54a09a6da68423425524788a1cbe1f2
Sheel Shah <sheelshah@Sheels-MacBook-Pro.local>
2023-06-16 14:48:17 -0500

    first commit

---
+++ exercise2.py
@@ -0,0 +1,10 @@
+def square_foot ():
+    length = int(input("what is the length of the house? "))
+    width = int(input("what is the width of the house? "))
+    square_footage = length * width
+    print(square_footage)
+    exit()
+square_foot()
+
+
+
---
+++ exercise2circumference.py
@@ -0,0 +1,9 @@
+import math
+pi = (math.pi)
+
+def circumference ():
+    radius = int(input("what is the radius of the circle? "))
+    circum_circle = 2*pi*radius
+    print(circum_circle)
+    exit()
+circumference()
